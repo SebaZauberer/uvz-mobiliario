873fd1e291f856cdc10bbea8db47e3ee4caf91de
Sebastian Olate H <seba.olate.h@gmail.com>
2017-09-03 19:04:29 -0300

    modificaciones modal y sus sliders

--- js/agency.js
+++ js/agency.js
@@ -43,5 +43,19 @@
             fade: 750,
             duration: 2500
         });
+    
+    //carousel-indicators bootstrap
+    var myCarousels = $(".carousel");
+	myCarousels.each(function( index, element  ) {
+		var myCarousel = $("#"+$(element).attr('id')); 
+		myCarousel.append("<ol class='carousel-indicators'></ol>");
+		var indicators = $("#"+$(element).attr('id') + " .carousel-indicators"); 
+		$("#"+$(element).attr('id') +" .carousel-inner").children(".item").each(function(index) {
+			console.log(index);
+			(index === 0) ? 
+			indicators.append("<li data-target='#"+$(element).attr('id')+"' data-slide-to='"+index+"' class='active'></li>") : 
+			indicators.append("<li data-target='#"+$(element).attr('id')+"' data-slide-to='"+index+"'></li>");
+		});
+	});
 
 })(jQuery); // End of use strict
